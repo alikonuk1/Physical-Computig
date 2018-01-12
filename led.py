6aaa47c8b3d6bf8dc361b1b409b253c1c17a807c
alkonuk <alikonuk1@gmail.com>
2018-01-12 19:15:33 +0300

    Update led.py

--- led.py
+++ led.py
@@ -2,9 +2,20 @@
  from time import sleep
 
 led = LED(17)
+led1 = LED(14)
+led2 = LED(15)
 
 while True:
     led.on()
     sleep(1)
     led.off()
+    led1.on()
     sleep(1)
+    led1.off()
+    led2.on()
+    sleep(1)
+    led2.off()
+    sleep(0)
+
+    
+    
